c674bab71ff0bca23254a1c9917ab2afd3df4a52
Łukasz Gurdek <git@ukasiu.pl>
2015-07-14 21:50:15 +0200

    autologin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,19 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  after_action :allow_iframe
+  before_action :check_user
+
+  def allow_iframe
+    response.headers.except! 'X-Frame-Options'
+  end
+
+  def check_user
+    unless current_user
+      store_location_for :user, url_for(:only_path => false)
+      redirect_to user_omniauth_authorize_path(provider: 'accounts_api')
+    end
+  end
 
   class NotAuthorized < Exception
   end
